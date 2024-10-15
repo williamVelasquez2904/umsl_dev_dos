8cf6aa5e62962c10f1cff9bb28f504e095dcd574
williamvelasquez2904 <wavm290472@gmail.com>
2024-10-14 21:27:27 -0400

    upd varios

--- app/enfermedad/cls/mEnfermedad.php
+++ app/enfermedad/cls/mEnfermedad.php
@@ -4,8 +4,15 @@
 	function __construct() {}
 
 	public function lista() {
-		$sql = "SELECT * FROM vw_banco WHERE banco_borrado=0 ORDER BY banco_ide DESC";
+		$sql = "SELECT * FROM vw_enfermedad WHERE enf_borrado=0 ORDER BY enf_ide DESC";
 		return Enlace::sql($sql,'',3,'');
 	}
 
+
+	public function insert() {
+		$sql = "SELECT sf_enf(?,?,?,?) AS res";
+		extract($_POST); $datos = array(0,Funciones::may($des),1,$_SESSION['s_usua_ide']);
+		//extract($_POST); $datos = array(0,'Pruebitas... ',1,$_SESSION['s_clien_ide']);
+		return Enlace::sql($sql,$datos,4,'res');
+	}
 } ?>
